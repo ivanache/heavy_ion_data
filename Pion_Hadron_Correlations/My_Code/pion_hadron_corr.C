e6a9bc798dc0e7902bf2d254511462e1d20b3596
The Admin <the_Admin@airbears2-10-142-77-208.airbears2.1918.berkeley.edu>
2017-10-21 00:21:40 -0700

    First complete pion-hadron correlation is done

--- Pion_Hadron_Correlations/My_Code/pion_hadron_corr.C
+++ Pion_Hadron_Correlations/My_Code/pion_hadron_corr.C
@@ -32,3 +32,137 @@ const int axis_pion_Ph1_lambda02= 9;
 const int axis_pion_Ph2_lambda02= 10;
 const int axis_pion_Ph1_dR      = 11;
 const int axis_pion_Ph2_dR      = 12;
+
+//variables of hPionTrack
+const int axis_corr_centrality  = 0;
+const int axis_corr_zvertex     = 1;
+const int axis_corr_triggerpT   = 2;
+const int axis_corr_trackpT     = 3;
+const int axis_corr_dphi        = 4;
+const int axis_corr_deta        = 5;
+const int axis_corr_zt          = 6;
+const int axis_corr_xi          = 7;
+const int axis_corr_mass        = 8;
+const int axis_corr_photon1Pt   = 9;
+const int axis_corr_photon2Pt   = 10;
+const int axis_corr_photon1M02  = 11;
+const int axis_corr_photon2M02  = 12;
+
+//variables of h_Cluster
+const int axis_Cluster_RunNumber = 0;
+const int axis_Cluster_Cen  = 1;
+const int axis_Cluster_Zvtx = 2;
+const int axis_Cluster_Pt   = 3;
+const int axis_Cluster_Eta  = 4;
+const int axis_Cluster_Phi  = 5;
+const int axis_Cluster_M02  = 6;
+const int axis_Cluster_Ncells = 7;
+const int axis_Cluster_Nmaxima = 8;
+const int axis_Cluster_DisToBorder = 9;
+const int axis_Cluster_DisToBad = 10;
+const int axis_Cluster_dR = 11;
+const int axis_Cluster_deta = 12;
+const int axis_Cluster_dphi = 13;
+const int axis_Cluster_Exoticity = 14;
+const int axis_Cluster_time = 15;
+const int axis_Cluster_nTracks =16;
+
+// Cutting function
+void SetCut(THnSparse* h, const int axis, double min, double max){
+    
+    int binmin = h->GetAxis(axis)->FindBin(min);
+    int binmax = h->GetAxis(axis)->FindBin(max);
+    h->GetAxis(axis)->SetRange(binmin, binmax-1);
+    return;
+}
+
+// 2D histogram dividing function
+// Precondition: the two histograms must have the same y- and x-dimensions
+TH2D* divide_histograms2D(TH2D* graph1, TH2D* graph2){
+    // Make the 2D histogram to contain the quotient and find minimum and maximum bins along both axes
+    TH2D* quotient = new TH2D(*graph1);
+    double x_bin_min = quotient->GetXaxis()->FindBin(quotient->GetXaxis()->GetXmin());
+    double x_bin_max = quotient->GetXaxis()->FindBin(quotient->GetXaxis()->GetXmax());
+    double y_bin_min = quotient->GetYaxis()->FindBin(quotient->GetYaxis()->GetXmin());
+    double y_bin_max = quotient->GetYaxis()->FindBin(quotient->GetYaxis()->GetXmax());
+    
+    // Loop over all bins, divide the element from graph 1 by its counterpart on graph 2
+    for(int i = x_bin_min; i <= x_bin_max; i++)
+        for(int j = y_bin_min; j <= y_bin_max; j++) {
+            if (graph2->GetBinContent(i, j) != 0)
+                quotient->SetBinContent(i, j, (graph1->GetBinContent(i, j))/(graph2->GetBinContent(i, j)));
+            // Failsafe
+            else
+                quotient->SetBinContent(i, j, 0);
+    }
+    
+    return quotient;
+}
+
+// Graphing and labeling function for TH2D histograms
+void graph2D(TH2D* graph, string title, string ylabel, string xlabel, double xseparation, double yseparation, TCanvas* canvas, string graph_option) {
+    // Set all titles, set all title separations
+    graph->SetTitle(Form("%s; %s; %s", title.c_str(), xlabel.c_str(), ylabel.c_str()));
+    graph->GetYaxis()->SetTitleOffset(yseparation);
+    graph->GetXaxis()->SetTitleOffset(xseparation);
+    
+    // Draw the graph
+    graph->Draw(graph_option.c_str());
+}
+
+// Main function
+void pion_hadron_corr() {
+    SetAtlasStyle();
+    TCanvas* canvas = new TCanvas();
+    
+    // Import data
+    TFile* input = new TFile("THnSparses_LHC13d_101517.root", "READ");
+    input->Print();
+    
+    // Get the THnSparses
+    THnSparse* hPionTrack = 0;
+    input->GetObject("h_PionTrack", hPionTrack);
+    THnSparse* hPionTrack_Mixed = 0;
+    input->GetObject("h_PionTrack_Mixed", hPionTrack_Mixed);
+    
+    // Cut the pion pT of both THnSparses to 10-12 GeV, the mass to 110-150 MeV, and track pT to 1-2 GeV
+    SetCut(hPionTrack, axis_corr_triggerpT, 10, 12);
+    SetCut(hPionTrack, axis_corr_mass, 0.11, 0.15);
+    SetCut(hPionTrack, axis_corr_trackpT, 1, 2);
+    
+    SetCut(hPionTrack_Mixed, axis_corr_triggerpT, 10, 12);
+    SetCut(hPionTrack_Mixed, axis_corr_mass, 0.11, 0.15);
+    SetCut(hPionTrack_Mixed, axis_corr_trackpT, 1, 2);
+    
+    // Make a 2D projection over both delta-phi and delta-eta for both THnSparses
+    TH2D* Pion_Track_Projection = hPionTrack->Projection(axis_corr_deta, axis_corr_dphi);
+    TH2D* Pion_Track_Mixed_Projection = hPionTrack_Mixed->Projection(axis_corr_deta, axis_corr_dphi);
+    
+    // Output the 2D projections
+    graph2D(Pion_Track_Projection, "Pion Track", "#Delta #eta", "#Delta #phi [rad]", 1.0, 1.0, canvas, "COLZ");
+    myText(.40,.92, kBlack, "Pion Track");
+    canvas->SaveAs("pion_track_graph.png");
+    canvas->Clear();
+    
+    graph2D(Pion_Track_Mixed_Projection, "Mixed Pion Track", "#Delta #eta", "#Delta #phi [rad]", 1.0, 1.0, canvas, "COLZ");
+    myText(.40,.92, kBlack, "Mixed Pion Track");
+    canvas->SaveAs("mixed_pion_track_graph.png");
+    canvas->Clear();
+    
+    // Get and graph the quotient of all bins from the pion track divided by all bins from the mixed pion track
+    // This is the correlation function
+    // Use both a surface plot and a 2D intensity chart
+    TH2D* correlation_function = divide_histograms2D(Pion_Track_Projection, Pion_Track_Mixed_Projection);
+    
+    graph2D(correlation_function, "Correlation Function", "#Delta #eta", "#Delta #phi [rad]", 1.0, 1.0, canvas, "COLZ");
+    myText(.40,.92, kBlack, "Correlation Function");
+    canvas->SaveAs("correlation_function_intensitychart.png");
+    
+    graph2D(correlation_function, "Correlation Function", "#Delta #eta", "#Delta #phi [rad]", 1.0, 1.0, canvas, "SURF2");
+    myText(.40,.92, kBlack, "Correlation Function");
+    canvas->SaveAs("correlation_function_surfaceplot.png");
+    
+    
+    
+    canvas->Close();
+}
---
+++ Pion_Hadron_Correlations/README.md
@@ -0,0 +1,7 @@
+# Pion Hadron Correlations
+
+## The portion of the project started on Oct. 17, 2017, which concerns pion hadron correlations
+
+### Technical Specifications
+- Miguel_Sample_Code: The code sent by Dr. Miguel Arratia on which the author's own code is partially based on
+- My_Code: The author's own code, which runs the correlation itself
